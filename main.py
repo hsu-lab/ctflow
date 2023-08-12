1ea43b07d67ad8b799469a4f4169e66f66aa97e8
Anil Yadav <ayadav01@ucla.edu>
2023-08-11 19:27:14 -0700

    adding main function

---
+++ main.py
@@ -0,0 +1,6 @@
+import ctflow.run_inference as ct_infer
+
+
+if __name__ == '__main__':
+    path_to_test_data = '/path_to_test_set'
+    ct_infer.execute(path_to_test_data, data_ext='data_format_of_test_set', tau=0.8, conf='mapping_conditoin', use_gpu=[], out_to='path_to_save_results')
